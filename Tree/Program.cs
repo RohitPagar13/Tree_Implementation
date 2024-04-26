cc6fd09b27cd6b0ce4257d4c69598a0da11ff332
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-26 11:16:44 +0530

    [Rohit] Add: Welcome to the Tree

---
+++ Tree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tree Structure");
+        }
+    }
+}

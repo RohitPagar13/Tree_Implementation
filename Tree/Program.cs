7fec4d1416efcdaef3192efab6cd588a02bf9a23
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-26 16:19:29 +0530

    [Rohit] Add: add Element function created

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -5,6 +5,39 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Tree Structure");
+            Tree t = new Tree();
+            while (true)
+            {
+                Console.WriteLine("\nWhat you want to do?\n1: Add Element\n0: Exit");
+                int choice= Convert.ToInt32(Console.ReadLine());
+                if (choice == 0)
+                {
+                    Console.WriteLine("Exiting..");
+                    break;
+                }
+                else
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter data to add");
+                            int data= Convert.ToInt32(Console.ReadLine());
+                            t.addElement(data);
+                            break;
+
+                        case 2:
+
+                            break;
+
+                        case 3:
+
+                            break;
+
+
+                    }
+                }
+            }
+            
         }
     }
 }
---
+++ Tree/Tree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    internal class Tree
+    {
+        List<Node> tree;
+        Node node;
+        int c; 
+        
+
+        public Tree()
+        {
+            tree = new List<Node>();
+            c = 0;
+        }
+
+        public void addElement(int data)
+        {
+            if(tree.Count == 0)
+            {
+                tree.Add(new Node(data));
+                Console.WriteLine("Element added: " + data);
+            }
+            else
+            {
+                Node n = tree[c];
+                if (n.getData() > data && n.getLeft() == null)
+                {
+                    tree.Add(new Node(data));
+                    Console.WriteLine("Element added: " + data);
+                }
+                else if (n.getData() < data && n.getRight() == null)
+                {
+                    tree.Add(null);
+                    tree.Add(new Node(data));
+                    Console.WriteLine("Element added: "+ data);
+                }
+                else
+                {
+                    c++;
+                    addElement(data);
+                }
+            }
+        }
+
+        
+    }
+}
